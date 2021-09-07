0559d6338b06199126db1124e598dea9be60a2a4
IvanSJr <ivan.junior2706@gmail.com>
2021-09-06 22:14:22 -0300

    Adicionando componentes basicos da aplicacao.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import NavBar from "components/navbar/NavBar";
+import Footer from "components/footer/Footer";
+import DataTable from "components/datatable/DataTable";
+
 function App() {
     return (
-        <div>
+        <>
+        <div className="container">
+            <NavBar></NavBar>
             <h1 className="text-primary">Olá Mundo!</h1>
+            <DataTable></DataTable>
         </div>
+        <Footer></Footer>
+        </>
     );
 }
 
