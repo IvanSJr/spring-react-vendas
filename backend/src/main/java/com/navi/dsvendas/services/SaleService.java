a0e81264ffb90e1ce7b670d50ff9c5fb4364caf4
Ivan Júnior <ivan.junior2706@gmail.com>
2021-09-08 09:59:23 -0300

    Pagination Sales.

---
+++ backend/src/main/java/com/navi/dsvendas/services/SaleService.java
@@ -0,0 +1,31 @@
+package com.navi.dsvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.navi.dsvendas.dto.SaleDTO;
+import com.navi.dsvendas.entities.Sale;
+import com.navi.dsvendas.repositories.SaleRepository;
+import com.navi.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository saleRepository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<Sale> list = saleRepository.findAll(pageable);
+		Page<SaleDTO> listDto = list.map(x -> new SaleDTO(x));
+		return listDto;
+	}
+	
+}
